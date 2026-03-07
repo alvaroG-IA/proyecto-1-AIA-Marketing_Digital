8dc5970a72fed27d28d671c5d452247c7cc17fe8
Alfon <alfonsoarmentamartin@gmail.com>
2026-03-07 13:52:30 +0100

    modelos y datos limpios para entrenamiento

--- parametros.py
+++ parametros.py
@@ -1,15 +1,12 @@
 import kagglehub
 import pandas as pd
-import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, IsolationForest
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
 import shutil
 import os
 from imblearn.over_sampling import SMOTE
 from collections import Counter
-from sklearn.preprocessing import StandardScaler
 
 # Obtener la ruta donde kagglehub guarda los datos
 path = kagglehub.dataset_download("henrysue/online-shoppers-intention")
@@ -48,7 +45,7 @@ y = y.astype(int)
 X_encoded = pd.get_dummies(df.drop('Revenue', axis=1), columns=['Month', 'VisitorType'], drop_first=True)
 X_encoded['Weekend'] = X_encoded['Weekend'].astype(int)
 
-X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
 
 smote = SMOTE(random_state=42)
 
@@ -57,30 +54,25 @@ X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
 print(f"Distribución original: {Counter(y_train)}")
 print(f"Nueva distribución: {Counter(y_train_res)}")
 
-print('1) Usando Random Forest:')
-rf = RandomForestClassifier(random_state=42, criterion='gini', n_estimators=200, max_depth=None, min_samples_split=2)
-rf.fit(X_train_res, y_train_res)
-probs = rf.predict_proba(X_test)[:, 1]
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5],
+    'criterion': ['gini', 'entropy']
+}
 
-nuevo_umbral = 0.5
-pred_umbral = (probs >= nuevo_umbral).astype(int)
+grid_search = GridSearchCV(
+    estimator=RandomForestClassifier(random_state=42),
+    param_grid=param_grid,
+    scoring='f1',
+    cv=3,
+    verbose=1,
+    n_jobs=-1
+)
 
-print(classification_report(y_test, pred_umbral))
+# Entrenamos con los datos de SMOTE
+grid_search.fit(X_train_res, y_train_res)
 
-print('2) Usando XGBoost:')
-xgbmodel = xgb.XGBClassifier(random_state=42)
-xgbmodel.fit(X_train_res, y_train_res)
-probs = xgbmodel.predict_proba(X_test)[:, 1]
-
-nuevo_umbral = 0.5
-pred_umbral_2 = (probs >= nuevo_umbral).astype(int)
-
-print(classification_report(y_test, pred_umbral_2))
-
-# Tratándolo como que la compra sea un Outlier
-print('3) Usando Isolation Forest')
-iso_forest = IsolationForest(contamination=0.15, random_state=42)
-iso_forest.fit(X_train, y_train)
-pred_3 = iso_forest.predict(X_test)
-pred_3_final = [1 if x == -1 else 0 for x in pred_3]
-print(classification_report(y_test, pred_3_final))
+# El mejor modelo encontrado
+best_rf = grid_search.best_estimator_
+print(f"Mejores parámetros: {grid_search.best_params_}")
